3881e37f4a00974583df9c8e92a0e57b0ed5500c
Ahmed <ahmedelsharawy117@gmail.com>
2024-12-02 23:53:53 +0200

    feat: handle errors and not found

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { errorHandler, notFound } from "./middleware/errorHandler";
 
 const app = express();
 
@@ -7,5 +8,8 @@ const PORT = process.env.PORT || 8080;
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
+
+app.use("*", notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`[server]: Listening on localhost:${PORT}`));
