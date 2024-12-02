0b70389b28675463c2c5f7e66336f1388ebf7aa7
Ahmed <ahmedelsharawy117@gmail.com>
2024-12-03 00:01:36 +0200

    feat: morgan logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
+import morgan from "morgan";
 import { errorHandler, notFound } from "./middleware/errorHandler";
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
