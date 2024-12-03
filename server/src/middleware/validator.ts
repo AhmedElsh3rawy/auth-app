5096a45f2d2019561b62c829fcb29759c2a3da28
Ahmed <ahmedelsharawy117@gmail.com>
2024-12-03 21:54:57 +0200

    feat: login validator

--- server/src/middleware/validator.ts
+++ server/src/middleware/validator.ts
@@ -4,12 +4,12 @@ import { fromZodError } from "zod-validation-error";
 import APIError from "../utils/APIError";
 import { registerSchema, loginSchema } from "../utils/validationSchema";
 
-export const registerValidator = (
+export const registerValidator = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const result = registerSchema.safeParse(req.body);
+  const result = await registerSchema.safeParseAsync(req.body);
   if (!result.success) {
     return next(new APIError(fromZodError(result.error), 400));
   }
@@ -21,4 +21,11 @@ export const loginValidator = (
   req: Request,
   res: Response,
   next: NextFunction,
-) => { };
+) => {
+  const result = loginSchema.safeParse(req.body);
+  if (!result.success) {
+    return next(new APIError(fromZodError(result.error), 400));
+  }
+  req.body = result.data;
+  next();
+};
